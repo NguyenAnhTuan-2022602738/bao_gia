d3f88f0c24877545339d7a17347113a4ba5eada8
NguyenAnhTuan-2022602738 <162803332+NguyenAnhTuan-2022602738@users.noreply.github.com>
2025-06-24 11:40:38 +0700

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Product = require("../models/product"); // Sửa đường dẫn
+const Product = require("/var/task/models/product"); // Sửa đường dẫn
 
 function getPriceTypeLabel(type) {
   switch (type) {
